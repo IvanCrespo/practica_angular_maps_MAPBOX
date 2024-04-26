525fb3f737f3aed3a5170f8c80ee2d09cdec171f
IvanCrespo <contacto.ivancrespo@gmail.com>
2024-04-26 17:59:08 -0600

    Mapbox Mapas

---
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
+
+@Component({
+  selector: 'map-mini-map',
+  templateUrl: './mini-map.component.html',
+  styleUrls: ['./mini-map.component.css']
+})
+export class MiniMapComponent implements AfterViewInit {
+
+  @Input() lngLat?: [number, number];
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit() {
+    if (!this.divMap?.nativeElement) throw new Error('Map Div not found!');
+    if (!this.lngLat) throw new Error('Lnglat can´t be null!');
+
+    // Mapa
+    const map = new Map({
+      container: this.divMap?.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false
+    });
+
+    new Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map);
+  }
+}
